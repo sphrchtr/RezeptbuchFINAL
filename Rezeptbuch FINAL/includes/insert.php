2da638e32618f0458679cdf7d31cd1e1ce6f5caa
TomiiBoii <45168820+TomiiBoii@users.noreply.github.com>
2019-01-10 19:11:39 +0100

    Update insert.php

--- Rezeptbuch FINAL/includes/insert.php
+++ Rezeptbuch FINAL/includes/insert.php
@@ -1,15 +1,14 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "rezeptbuch");
+/* Versucht MQSQL Verbindung. */
+$link = mysqli_connect("localhost", "root", "", "rezeptbuch");                                       // Neue Connection zur DB
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error());                                     // Mögliche Error Meldung falls ein Connection Error kommt
 }
  
-// Escape user inputs for security
-$namerecipe = mysqli_real_escape_string($link, $_REQUEST['namerecipe']);
+
+$namerecipe = mysqli_real_escape_string($link, $_REQUEST['namerecipe']);                            // Holt sich alle Werte aus der Tabelle und trägt sie in die Variablen ein
 $preparation = mysqli_real_escape_string($link, $_REQUEST['preparation']);
 $difficulty = mysqli_real_escape_string($link, $_REQUEST['difficulty']);
 $ingredient1 =  mysqli_real_escape_string($link, $_REQUEST['ingredient1']);
@@ -29,20 +28,20 @@ $time = mysqli_real_escape_string($link, $_REQUEST['time']);
 $amount = mysqli_real_escape_string($link, $_REQUEST['amount']);
 $source = mysqli_real_escape_string($link, $_REQUEST['source']);
  
-// Attempt insert query execution
+// Versucht Insert Query
 $sql = "INSERT INTO rezepte (namerecipe, ingredient1 , ingredient2 , ingredient3 , ingredient4 , ingredient5 , ingredient6 , ingredient7 , ingredient8 , ingredient9 , ingredient10 , preparation, cathegory1 , cathegory2 , cathegory3 , difficulty, time , amount ,  source ) VALUES ('$namerecipe','$ingredient1','$ingredient2','$ingredient3','$ingredient4','$ingredient5','$ingredient6','$ingredient7','$ingredient8','$ingredient9','$ingredient10','$preparation', '$cathegory1','$cathegory2','$cathegory3','$difficulty','$time','$amount', '$source')";
-if(mysqli_query($link, $sql)){
+if(mysqli_query($link, $sql)){                                                                      // Query mit Link und SQL
  /*   header("Location:../Rezeptform.php");
         exit; */
-    $location = "../Rezeptform.php";
-    header("Location: $location?message=success");
-if(!empty($_GET['message'])) {
-    $message = $_GET['message'];
+    $location = "../Rezeptform.php";                                                                // Variable wird festgelgt
+    header("Location: $location?message=success");                                                  // Weiterleitung auf Alle_Rezepte.php
+if(!empty($_GET['message'])) {                                                                      // Wenn Message leer dann
+    $message = $_GET['message'];                                                                    // Message aus Adress Zeile bekommen
 } 
-else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+else{                         
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);                           // Mögliche Fehlermeldung falls Daten nicht geupdatet werden konnten.
 }
 }
 // Close connection
-mysqli_close($link);
+mysqli_close($link);                                                                                // Link zu DB schließen
 ?>
