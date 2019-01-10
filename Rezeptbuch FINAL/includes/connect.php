1a018743f696640703a4bdf137015108be3914c6
TomiiBoii <45168820+TomiiBoii@users.noreply.github.com>
2019-01-10 19:22:50 +0100

    Update connect.php

--- Rezeptbuch FINAL/includes/connect.php
+++ Rezeptbuch FINAL/includes/connect.php
@@ -1,7 +1,7 @@
 <?php
-$link = mysqli_connect('localhost', 'root', '');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
+$link = mysqli_connect('localhost', 'root', '');                                            // Neue verbindung zur Datenbank
+if (!$link) {                                                                               // Mögliche Fehlermeldung falls Verbindung abbricht 
+    die('Could not connect: ' . mysql_error());                                             // Error
 }
-mysqli_close($link);
+mysqli_close($link);                                                                        // Verbindung close
 ?>
