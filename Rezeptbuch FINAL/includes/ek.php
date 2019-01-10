26a7a15b6b556fad51ed851ba6c4893f3f800f16
badihavgji <31517390+badihavgji@users.noreply.github.com>
2019-01-10 19:03:53 +0100

    Update ek.php

--- Rezeptbuch FINAL/includes/ek.php
+++ Rezeptbuch FINAL/includes/ek.php
@@ -1,18 +1,18 @@
 <?php
-$link = mysqli_connect("localhost", "root", "", "rezeptbuch");
+$link = mysqli_connect("localhost", "root", "", "rezeptbuch"); // Verbingung zur Datenbank mit Benutzerdaten
  
-// Check connection
+// Verbindung wird geprüft mit Fehlermeldung
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$ingredient = $_GET['ingredient'];
+$ingredient = $_GET['ingredient']; //Werte (ingredient, id) werden von view.php übergegeben 
 $id = $_GET['id'];
 
-$sql = "INSERT INTO Einkaufsliste (Name) VALUES ('$ingredient')";
+$sql = "INSERT INTO Einkaufsliste (Name) VALUES ('$ingredient')"; //Werte werden in Datenbank "einkaufsliste" eingefügt
 
 if(mysqli_query($link, $sql)){
-    header("Location: ./view.php?id=$id");
+    header("Location: ./view.php?id=$id"); //Weiterleitung zur view.php
 }
 
 else{
