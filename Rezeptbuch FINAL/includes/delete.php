ba7b273e09ab7c1dbde9a4fd10116e0ceefb7696
TomiiBoii <45168820+TomiiBoii@users.noreply.github.com>
2019-01-10 19:21:32 +0100

    Update delete.php

--- Rezeptbuch FINAL/includes/delete.php
+++ Rezeptbuch FINAL/includes/delete.php
@@ -1,8 +1,8 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=rezeptbuch', 'root', '');
-$id = $_GET['id'];
-$statement = $pdo->prepare("DELETE FROM rezepte WHERE id = ?");
-$statement->execute(array($id));
-header("Location:../Alle_Rezepte.php");
+$pdo = new PDO('mysql:host=localhost;dbname=rezeptbuch', 'root', '');               // Neue Verbindung zur Datenbank ( Idee von PHP-Einfach.de)
+$id = $_GET['id'];                                                                  // Get Id aus Adress Zeile            
+$statement = $pdo->prepare("DELETE FROM rezepte WHERE id = ?");                     // SQL wird mit der Id Variabel vervollständigt
+$statement->execute(array($id));                                                    // Query wird ausgeführt
+header("Location:../Alle_Rezepte.php");                                             // Weiterleitung zur Alle_Rezepte.php
     exit;
 ?>
